886cc009363fa53fe3335e238cf96473adc53327
Thiago Cerutti <thiagocerutti@gmail.com>
2024-11-28 21:20:02 -0300

    feat: sticky header

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,15 @@ import ListaEducandos from "@/app/components/ListaEducandos";
 import ListaInsignias from "./components/ListaInsignias";
 import styles from "./page.module.css";
 import GraficoInsigniasConquistadas from "./components/GraficoInsigniasConquistadas/GraficoInsigniasConquistadas";
+import paqLogo from "./img/paq-logo.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>PAQ Dashboard</h1>
+      <h1>
+        <Image src={paqLogo} alt="logo do PAQ" /> PAQ Dashboard
+      </h1>
       <main>
         <div>
           <ListaEducandos />
